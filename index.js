f7a301298138f73dc509626ef4d29211d85b6a27
Colton Brenneman <colton.brenneman@gmail.com>
2022-12-15 23:46:04 -0700

    commit tree 5945d548d78104620bbe043edd5f175915984ecf

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers){
+    return (drivers.slice(0,2))
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return (drivers.slice(-2))
+}
+
+const createFareMultiplier = function(numberOfTickers){
+    return function(fare){
+        return numberOfTickers * fare;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(drivers){
+    return drivers.slice(-2)
+}
